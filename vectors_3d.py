6fb26fa60a789e301fb1bd03d5059a2f4a98d045
Steve Bentley <steve.bentley@greatstate.co>
2019-02-26 13:26:28 +0000

    Add dot product

--- vectors_3d.py
+++ vectors_3d.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def add(*vectors):
     by_coordinate = zip(*vectors)
     coordinate_sums = [sum(coords) for coords in by_coordinate]
@@ -7,6 +10,16 @@ def add(*vectors):
 def scale(s, vector):
     return tuple(v * s for v in vector)
 
+
+def length(v):
+    return sqrt(sum([coord ** 2 for coord in v]))
+
+
+def dot(vector1, vector2):
+    return sum([v1 * v2 for v1, v2 in zip(vector1, vector2)])
+
 if __name__ == "__main__":
     print(add((1, 2, 3), (4, 5, 6), (7, 8, 9), (10, 11, 12)))
     print(scale(5, (1, 2, 3)))
+    print(length((3, 4, 12)))  # == 13
+    print(dot((1, 2, -1), (3, 0, 3)))  # == 0
